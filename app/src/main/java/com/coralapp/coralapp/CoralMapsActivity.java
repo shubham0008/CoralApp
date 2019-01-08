6288777ecef7ec88c3893592c1ae40c81d4978c0
the-Alchemister <shubhamkachroo@yahoo.co.in>
2019-01-09 05:05:28 +0530

    Maps Activity Revised and Feature Activity modified.

--- app/src/main/java/com/coralapp/coralapp/CoralMapsActivity.java
+++ app/src/main/java/com/coralapp/coralapp/CoralMapsActivity.java
@@ -1,8 +1,15 @@
 package com.coralapp.coralapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,7 +22,7 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CoralMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CoralMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -44,22 +51,42 @@ public class CoralMapsActivity extends FragmentActivity implements OnMapReadyCal
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(37.4, -122.1);
+        LatLng markerStarter = new LatLng(12.33, 92.66);
+        LatLng markerWandur = new LatLng(11.48333, 92.58333);
+        LatLng markerMayabandar = new LatLng(12.88333, 92.98333);
+        LatLng markerNorthReef = new LatLng(13.11667, 92.71667);
 
-        Circle circle = mMap.addCircle(new CircleOptions()
-                .center(new LatLng(37.4, -122.1))
-                .radius(100000)
+        final Circle circle1 = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(11.48333, 92.58333))
+                .radius(10000)
                 .strokeWidth(10)
                 .strokeColor(Color.GREEN)
                 .fillColor(Color.argb(128, 255, 0, 0))
                 .clickable(true));
 
+        Circle circle2 = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(12.88333, 92.98333))
+                .radius(10000)
+                .strokeWidth(10)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(128, 255, 0, 0))
+                .clickable(true));
+
+        Circle circle3 = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(13.11667, 92.71667))
+                .radius(10000)
+                .strokeWidth(10)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(128, 255, 0, 0))
+                .clickable(true));
 
         mMap.setOnCircleClickListener(new GoogleMap.OnCircleClickListener() {
             @Override
             public void onCircleClick(Circle circle) {
-                Toast.makeText(CoralMapsActivity.this, "Circle working Bro", Toast.LENGTH_SHORT).show();
-
+                Double currLat = circle.getCenter().latitude;
+                Double currLong = circle.getCenter().longitude;
+                //Toast.makeText(CoralMapsActivity.this, "" + currLat + currLong, Toast.LENGTH_SHORT).show();
+                openDialog(circle);
             }
         });
 //        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -69,10 +96,75 @@ public class CoralMapsActivity extends FragmentActivity implements OnMapReadyCal
 //
 //            }
 //        });
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        CameraPosition target = CameraPosition.builder().target(sydney).zoom((float) 6.6).bearing(82).build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(target));
 
+        mMap.addMarker(new MarkerOptions().position(markerWandur).title("Marker in Wandur"));
+        mMap.addMarker(new MarkerOptions().position(markerMayabandar).title("Marker in Mayabandar"));
+        mMap.addMarker(new MarkerOptions().position(markerNorthReef).title("Marker in NorthReef"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(markerStarter));
+        CameraPosition target = CameraPosition.builder().target(markerStarter).zoom((float) 7.5).bearing(82).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(target));
     }
+
+    public void openDialog(final Circle circle) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        // Set Custom Title
+        TextView title = new TextView(this);
+        // Title Properties
+        title.setText("Do you want to select this area?");
+        title.setPadding(10, 30, 10, 20);   // Set Position
+        title.setGravity(Gravity.CENTER);
+        title.setTextColor(Color.BLACK);
+        title.setTextSize(20);
+        alertDialog.setCustomTitle(title);
+
+        // Set Message
+        TextView msg = new TextView(this);
+        // Message Properties
+        msg.setText("Press OK to freeze this region to dive!");
+        msg.setGravity(Gravity.CENTER_HORIZONTAL);
+        msg.setTextColor(Color.BLACK);
+        alertDialog.setView(msg);
+
+        // Set Button
+        // you can more buttons
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,"OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // Perform Action on Button
+                Double currLat = circle.getCenter().latitude;
+                Double currLong = circle.getCenter().longitude;
+                circle.setFillColor(Color.argb(128, 0, 255, 0));
+                Toast.makeText(CoralMapsActivity.this, "Co-ords:" + currLat + " and "+ currLong, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // Perform Action on Button
+                Toast.makeText(CoralMapsActivity.this, "You rejected the job!", Toast.LENGTH_SHORT).show();
+                circle.setFillColor(Color.argb(128, 255, 0, 0));
+            }
+        });
+
+        new Dialog(getApplicationContext());
+        alertDialog.show();
+
+        // Set Properties for OK Button
+        final Button okBT = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+        LinearLayout.LayoutParams neutralBtnLP = (LinearLayout.LayoutParams) okBT.getLayoutParams();
+        neutralBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        okBT.setPadding(50, 10, 10, 10);   // Set Position
+        okBT.setTextColor(Color.BLUE);
+        okBT.setLayoutParams(neutralBtnLP);
+
+        final Button cancelBT = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        LinearLayout.LayoutParams negBtnLP = (LinearLayout.LayoutParams) okBT.getLayoutParams();
+        negBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        cancelBT.setTextColor(Color.RED);
+        cancelBT.setLayoutParams(negBtnLP);
+    }
+
 }
+
+
--- app/src/main/java/com/coralapp/coralapp/FeatureActivity.java
+++ app/src/main/java/com/coralapp/coralapp/FeatureActivity.java
@@ -1,25 +1,56 @@
 package com.coralapp.coralapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FeatureActivity extends AppCompatActivity {
+
+    CardView diveCV, reportCV, historyCV;
+    ImageView logoutIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature);
-        ImageView dive ;
-        dive = (ImageView) findViewById(R.id.iv_dive);
 
-        dive.setOnClickListener(new View.OnClickListener() {
+        diveCV = findViewById(R.id.cv_dive);
+        reportCV = findViewById(R.id.cv_report);
+        historyCV = findViewById(R.id.cv_history);
+
+        logoutIV = findViewById(R.id.iv_logout);
+
+        diveCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(FeatureActivity.this,CoralMapsActivity.class));
+                startActivity(new Intent(FeatureActivity.this, CoralMapsActivity.class));
+            }
+        });
+
+        reportCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeatureActivity.this, ReportActivity.class));
+            }
+        });
+
+        historyCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeatureActivity.this, HistoryActivity.class));
+            }
+        });
+
+        logoutIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeatureActivity.this, MainActivity.class));
+                finish();
+                Toast.makeText(FeatureActivity.this, "LOGOUT SUCCESSFUL", Toast.LENGTH_SHORT).show();
             }
         });
     }
